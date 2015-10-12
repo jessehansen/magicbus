113fad2af57c47770a77d1a360ccace2a88e39a0
Ethan Young <eyoung@leisurelink.com>
2015-10-12 12:05:18 -0600

    Add an integration test for the broker.

---
+++ test/broker.integration-tests.js
@@ -0,0 +1,38 @@
+'use strict';
+
+var Broker = require('../').Broker;
+
+var chai = require('chai');
+var expect = chai.expect;
+
+describe('Broker really using RabbitMQ', function() {
+  var appName = 'magicbus-tests';
+  var connectionInfo = {
+    server: 'localhost',
+    vhost: '/',
+    user: 'guest',
+    pass: 'guest'
+  };
+  var broker;
+
+  before(function() {
+    broker = new Broker(appName, connectionInfo);
+  });
+
+  it('should be able to publish and consume messages', function(done) {
+    var theMessage = 'Can I buy your magic bus?';
+
+    var handler = function(msg) {
+      var messageContent = new Buffer(msg.content).toString();
+      expect(messageContent).to.eq(theMessage);
+
+      broker.ack('subscribe', msg);
+
+      done();
+    };
+
+    broker.consume('subscribe', handler).then(function() {
+      broker.publish('publish', 'anything', new Buffer(theMessage));
+    });
+  });
+});
