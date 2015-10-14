0eca047a3b7260c48449b7b742c7d1a0f666ec6c
Ethan Young <ethankyoung@gmail.com>
2015-10-14 08:51:05 -0600

    Split Sender and Receiver into their own files and add an integration test.

---
+++ test/sendrec.integration-tests.js
@@ -0,0 +1,53 @@
+'use strict';
+
+var Broker = require('../').Broker;
+var Sender = require('../').Sender;
+var Receiver = require('../').Receiver;
+
+var chai = require('chai');
+var expect = chai.expect;
+
+describe('Send/Receive integration', function() {
+  var appName = 'magicbus-tests';
+  var connectionInfo = {
+    server: 'localhost',
+    vhost: '/',
+    user: 'guest',
+    pass: 'guest'
+  };
+  var broker;
+  var sender;
+  var receiver;
+
+  before(function() {
+    broker = new Broker(appName, connectionInfo);
+    sender = new Sender(broker, {
+      routeName: 'publish'
+    });
+    receiver = new Receiver(broker, {
+      routeName: 'subscribe'
+    });
+  });
+
+  after(function() {
+    broker.shutdown();
+  });
+
+  it('should be able to send a message and receive that message', function(done) {
+    var message = {
+      fooId: 123
+    };
+    var messageType = 'deactivateFooCommand';
+
+    var handler = function(handlerMessage, handlerMessageTypes) {
+      expect(handlerMessage).to.eql(message);
+      expect(handlerMessageTypes).to.eql([messageType]);
+
+      done();
+    };
+
+    receiver.startReceiving(handler).then(function() {
+      sender.send(message, messageType);
+    });
+  });
+});
