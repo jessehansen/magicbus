2697b7cdbc7285aeda3cb028649049832a0955b1
Jesse Hansen <twindagger@gmail.com>
2015-10-22 08:32:14 -0600

    Add consumer pipeline with supported actions

---
+++ test/middleware/consumer-pipeline.tests.js
@@ -0,0 +1,97 @@
+'use strict';
+
+var ConsumerPipeline = require('../../lib/middleware').ConsumerPipeline;
+
+var chai = require('chai');
+var chaiAsPromised = require('chai-as-promised');
+
+chai.use(chaiAsPromised);
+var expect = chai.expect;
+var assert = chai.assert;
+
+function simpleMiddleware(message, actions){
+  message.properties.headers.push('first: true');
+  actions.next();
+}
+function secondMiddleware(message, actions){
+  message.properties.headers.push('second: true');
+  actions.next();
+}
+function errorMiddleware(message, actions) {
+  actions.error(new Error('oh crap'));
+}
+
+describe('ConsumerPipeline', function() {
+  var consumerPipeline;
+  var message;
+
+  beforeEach(function() {
+    consumerPipeline = new ConsumerPipeline();
+    message = {properties: {headers:[]}, payload:'data'};
+  });
+
+  describe('#execute', function(){
+    it('should eventually fulfill promise', function(done){
+      expect(consumerPipeline.execute(message)).to.eventually.be.fulfilled.and.notify(done);
+    });
+    it('should call middleware function once when it is given one', function(done){
+      consumerPipeline.use(simpleMiddleware);
+      consumerPipeline.execute(message).then(() => {
+        expect(message.properties.headers.length).to.equal(1);
+        expect(message.properties.headers[0]).to.equal('first: true');
+        done();
+      }, function(){
+        assert.fail('expected promise to succeed, but it failed');
+        done();
+      });
+    });
+    it('should call middleware functions in succession when given multiple', function(done){
+      consumerPipeline.use(simpleMiddleware);
+      consumerPipeline.use(secondMiddleware);
+      consumerPipeline.execute(message).then(function(){
+        expect(message.properties.headers.length).to.equal(2);
+        expect(message.properties.headers[0]).to.equal('first: true');
+        expect(message.properties.headers[1]).to.equal('second: true');
+        done();
+      }, function(){
+        assert.fail('expected promise to succeed, but it failed');
+        done();
+      });
+    });
+    it('should reject promise when error occurs', function(done){
+      consumerPipeline.use(errorMiddleware);
+      expect(consumerPipeline.execute(message)).to.eventually.be.rejectedWith('oh crap').and.notify(done);
+    });
+    it('should not call successive functions when middleware errors', function(done){
+      consumerPipeline.use(errorMiddleware);
+      consumerPipeline.use(simpleMiddleware);
+      consumerPipeline.use(secondMiddleware);
+      consumerPipeline.execute(message).then(function(){
+        assert.fail('expected promise to fail, but it succeeded');
+        done();
+      }, function(){
+        expect(message.properties.headers.length).to.equal(0);
+        done();
+      });
+    });
+    var funcs = ['ack', 'nack', 'reject', 'reply'];
+    for (var i = 0; i < funcs.length; i++){
+      var fn = funcs[i];
+      it('should not call successive functions when middleware calls ' + fn, function(done){
+        consumerPipeline = new ConsumerPipeline();
+        consumerPipeline.use(function(msg, actions){
+          actions[fn]({});
+        });
+        consumerPipeline.use(simpleMiddleware);
+
+        consumerPipeline.execute(message).then(function(){
+          expect(message.properties.headers.length).to.equal(0);
+          done();
+        }, function(){
+          assert.fail('expected promise to succeed, but it failed');
+          done();
+        });
+      });
+    }
+  });
+});
