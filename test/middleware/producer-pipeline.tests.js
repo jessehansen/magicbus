5464c4be5bfc4f255b8570b58304d6801de48b72
Jesse Hansen <twindagger@gmail.com>
2015-10-22 08:50:02 -0600

    Don't use lambdas yet since our builder doesn't support them

--- test/middleware/producer-pipeline.tests.js
+++ test/middleware/producer-pipeline.tests.js
@@ -36,7 +36,7 @@ describe('ProducerPipeline', function() {
     });
     it('should call middleware function once when it is given one', function(done){
       producerPipeline.use(simpleMiddleware);
-      producerPipeline.execute(message).then(() => {
+      producerPipeline.execute(message).then(function(){
         expect(message.properties.headers.length).to.equal(1);
         expect(message.properties.headers[0]).to.equal('first: true');
         done();
