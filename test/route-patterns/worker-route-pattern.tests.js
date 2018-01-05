45caa8ef1474e4c0a7cbe4e3032d125e15d9ae15
Jesse Hansen <twindagger@gmail.com>
2018-01-05 13:27:41 -0700

    Fix some problems with the worker route pattern

--- test/route-patterns/worker-route-pattern.tests.js
+++ test/route-patterns/worker-route-pattern.tests.js
@@ -2,28 +2,22 @@ const workerRoutePattern = require('../../lib/route-patterns/worker-route-patter
 
 const Promise = require('bluebird')
 
-describe('WorkerRoutePattern', function () {
+describe('WorkerRoutePattern', () => {
   let mockTopology
   let routePattern
 
-  beforeEach(function () {
+  beforeEach(() => {
     mockTopology = {
-      createExchange: jest.fn(function () {
-        return Promise.resolve()
-      }),
-      createQueue: jest.fn(function () {
-        return Promise.resolve()
-      }),
-      createBinding: jest.fn(function () {
-        return Promise.resolve()
-      })
+      createExchange: jest.fn(() => Promise.resolve()),
+      createQueue: jest.fn(() => Promise.resolve()),
+      createBinding: jest.fn(() => Promise.resolve())
     }
 
     routePattern = workerRoutePattern()
   })
 
-  it('should assert a dead letter exchange', function () {
-    return routePattern(mockTopology, 'my-domain', 'my-app', 'my-route').then(function () {
+  it('should assert a dead letter exchange', () => {
+    return routePattern(mockTopology, 'my-domain', 'my-app', 'my-route').then(() => {
       expect(mockTopology.createExchange).toHaveBeenCalledWith({
         name: 'my-domain.my-app.my-route.failed',
         type: 'fanout',
@@ -32,16 +26,16 @@ describe('WorkerRoutePattern', function () {
     })
   })
 
-  it('should assert a queue to hold failed messages', function () {
-    return routePattern(mockTopology, 'my-domain', 'my-app', 'my-route').then(function () {
+  it('should assert a queue to hold failed messages', () => {
+    return routePattern(mockTopology, 'my-domain', 'my-app', 'my-route').then(() => {
       expect(mockTopology.createQueue).toHaveBeenCalledWith({
         name: 'my-domain.my-app.my-route.failed'
       })
     })
   })
 
-  it('should bind the failed message queue to the dead letter exchange', function () {
-    return routePattern(mockTopology, 'my-domain', 'my-app', 'my-route').then(function () {
+  it('should bind the failed message queue to the dead letter exchange', () => {
+    return routePattern(mockTopology, 'my-domain', 'my-app', 'my-route').then(() => {
       expect(mockTopology.createBinding).toHaveBeenCalledWith({
         source: 'my-domain.my-app.my-route.failed',
         target: 'my-domain.my-app.my-route.failed'
@@ -49,26 +43,39 @@ describe('WorkerRoutePattern', function () {
     })
   })
 
-  it('should assert the queue to consume from and connect it to the dead letter exchange', function () {
-    return routePattern(mockTopology, 'my-domain', 'my-app', 'my-route').then(function () {
+  it('should assert the queue to consume from and connect it to the dead letter exchange', () => {
+    return routePattern(mockTopology, 'my-domain', 'my-app', 'my-route').then(() => {
       expect(mockTopology.createQueue).toHaveBeenCalledWith({
         name: 'my-domain.my-app.my-route',
-        deadLetter: 'my-domain.my-app.my-route.failed'
+        deadLetter: 'my-domain.my-app.my-route.failed',
+        noAck: false
       })
     })
   })
 
-  it('should return the name of the queue to consume from', function () {
+  it('should return the name of the queue to consume from', () => {
     let p = routePattern(mockTopology, 'my-domain', 'my-app', 'my-route')
 
     return expect(p).resolves.toEqual({ queueName: 'my-domain.my-app.my-route' })
   })
 
-  it('should reject if any of the topology cannot be created', function () {
-    mockTopology.createQueue = function () {
+  it('should reject if any of the topology cannot be created', () => {
+    mockTopology.createQueue = () => {
       return Promise.reject(new Error('Nuts!'))
     }
 
     return expect(routePattern(mockTopology, 'my-domain', 'my-app', 'my-route')).rejects.toThrow('Nuts!')
+  })
+  describe('given options with noAck true', () => {
+    it('should assert a queue with noAck', () => {
+      routePattern = workerRoutePattern({ noAck: true })
+      return routePattern(mockTopology, 'my-domain', 'my-app', 'my-route').then(() => {
+        expect(mockTopology.createQueue).toHaveBeenCalledWith({
+          name: 'my-domain.my-app.my-route',
+          deadLetter: 'my-domain.my-app.my-route.failed',
+          noAck: true
+        })
+      })
+    })
   })
 })
